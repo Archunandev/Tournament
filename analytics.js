c5f137b2ccb68b02dad0cd6621b384f1ecb98454
Vertextrn <108271300+Vertextrn@users.noreply.github.com>
2022-12-16 13:27:41 +0530

    Update and rename 2af-3gdhd-gshs.html to analytics.js

---
+++ analytics.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCnmUuCMskMyugVmATZa_VAQ5amXCx8RBo",
+    authDomain: "playunitegg.firebaseapp.com",
+    projectId: "playunitegg",
+    storageBucket: "playunitegg.appspot.com",
+    messagingSenderId: "566300753813",
+    appId: "1:566300753813:web:39a38e5c9c48c6685ac09c",
+    measurementId: "G-0NB58H6Z91"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
